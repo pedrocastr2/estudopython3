8d99cd2441fc4a197e9a3bc9ee844b391070a199
pedrocastr <102086416+pedrocastr2@users.noreply.github.com>
2024-09-11 16:35:35 -0300

    Add files via upload

---
+++ operaesmatems.py
@@ -0,0 +1,21 @@
+num1 = 3
+num2 = 2
+num3 = 1.5
+
+print("Opeações")
+print("--------------------")
+print("num1 + num2 = ",num1 + num2)
+print("num1 - num2 = ",num1 - num2)
+print("num1 * num2 = ",num1 * num2)
+print("num1 / num2 = ",num1 / num2)
+print("num1 % num2 = ",num1 % num2)
+print("num1 ** num2 =",num1 ** num2)
+print("num1 // num2 = ",num1 // num2)
+print("--------------------")
+print( 3 + 5 * 7 +3)
+
+
+print("--------------------")
+print("absoluto de -15 é ",abs(-15))
+
+
